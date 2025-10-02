c096e66ec08209638b3a0ec081b418739c2e72cf
Huanzhou <hz04@ic.ac.uk>
2025-10-02 11:10:24 +0000

    add arg parser

--- src/rag/simple_rag.py
+++ src/rag/simple_rag.py
@@ -1,5 +1,6 @@
 import sys
 import asyncio
+import argparse
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -44,17 +45,28 @@ async def encode_pdf(path, chunk_size=1000, chunk_overlap=200):
 
     return vectorstore
 
-async def main(path):
+async def main(args):
+    path = args.path
+    query = args.query
     chunks_vector_store = await encode_pdf(path, chunk_size=1000, chunk_overlap=200)
     chunks_query_retriever = chunks_vector_store.as_retriever(search_kwargs={"k": 2})
-    test_query = "What is the main cause of climate change?"
-    context = retrieve_context_per_question(test_query, chunks_query_retriever)
+    context = retrieve_context_per_question(query, chunks_query_retriever)
     show_context(context)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Encode a PDF document and test a simple RAG.")
+    parser.add_argument("--path", type=str, default="data/Understanding_Climate_Change.pdf",
+                        help="Path to the PDF file to encode.")
+    parser.add_argument("--query", type=str, default="What is the main cause of climate change?",
+                        help="Query to test the retriever (default: 'What is the main cause of climate change?').")
+
+    # Parse and validate arguments
+    return parser.parse_args()
 
 if __name__ == "__main__":
     import sys
     from dotenv import load_dotenv
     load_dotenv()
 
-    path = "data/Understanding_Climate_Change.pdf"
-    asyncio.run(main(path))
+    args = parse_args()
+    asyncio.run(main(args))
